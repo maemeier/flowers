651458d630705a5c3faa4160680f85595d44ae1c
Mae Kangsadan <maemeier@live.com>
2020-07-08 01:53:38 +0200

    delete component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/places" component={Places} />
